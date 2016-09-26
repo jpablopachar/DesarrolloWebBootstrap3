3f596317a957127be2b49a9025ff81f1d0797f9d
Juan Pablo Pachar Viñan <jpablopachar1993@gmail.com>
2016-09-26 14:44:09 -0500

    Se agrego nuevas carpetas css y js y el framework Bootstrap y jquery

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Workshops con Bootstrap 3</title>
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+    </head>
+    <body>
+        <h1>Bootstrap ejemplo</h1>
+        <p>Hola mundo.</p>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery-1.12.4.min.js"></script>
+    </body>
 </html>
