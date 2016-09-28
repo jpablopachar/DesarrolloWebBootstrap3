be28530d902d1eb8f5d222ac40ebd32dc81e2853
Juan Pablo Pachar Viñan <jpablopachar1993@gmail.com>
2016-09-28 12:56:09 -0500

    Creación de una etiqueta header e ingreso el contenido de formadores

--- detail.php
+++ detail.php
@@ -2,14 +2,60 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Workshop Primer Detalle</title>
+        <title>Workshop Primeros Detalles</title>
         <link rel="stylesheet" href="css/styles.css">
         <?php include 'inc/headCommon.php'; ?>
     </head>
     <body>
         <?php include 'inc/header.php'; ?>
-        <h1>Bootstrap ejemplo</h1>
-        <p>Hola mundo.</p>
+        <article id="w1-detail" class="detail">
+            <header>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <h2>Desarrolla front-end like a boss aprendiendo Bootstrap</h2>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-7">
+                        <section id="intro">
+                            <h3>En este workshop aprenderás...</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum voluptatum qui sapiente temporibus sunt exercitationem doloribus, aliquid facere vel id nostrum ex quae, autem repudiandae laudantium dolorem repellendus aperiam at culpa accusamus veniam assumenda, blanditiis. Unde dolores reiciendis qui illo!</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus similique, aliquam incidunt animi aspernatur officiis sequi doloribus, cum cupiditate quis praesentium, non impedit nemo eaque.</p>
+                        </section>
+                        <section id="trainer-list">
+                            <h3>Los formadores</h3>
+                            <div class="trainer row">
+                                <div class="col-xs-4">
+                                    <img src="img/jpablo.jpg" alt="Juan Pablo Pachar">
+                                </div>
+                            </div>
+                            <div class="col-xs-8">
+                                <h4>Juan Pablo Pachar</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint repellendus non ipsum ad debitis, quod nobis!<br>Estudiante Universitario</p>
+                                <a href="https://www.facebook.com/jpablo.pachar" class="trainer-link">CV online</a><a href="https://twitter.com/jpablopacharLDU" class="trainer-link">@jpablopacharLDU</a>
+                            </div>
+                            <div class="trainer row">
+                                <div class="col-xs-4">
+                                    <img src="img/junior.jpg" alt="Junior Vásquez">
+                                </div>
+                            </div>
+                            <div class="col-xs-8">
+                                <h4>Junior Vásquez</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint repellendus non ipsum ad debitis, quod nobis!<br>Estudiante Universitario de la UTPL</p>
+                                <a href="https://www.facebook.com/eljunior.escobar1?fref=ts" class="trainer-link">CV online</a>
+                            </div>
+                        </section>
+                    </div>
+                    <div class="col-xs-5">
+                    
+                    </div>
+                </div>
+            </div>
+        </article>
         <?php include 'inc/footer.php'; ?>
         <?php include 'inc/footerCommon.php'; ?>
     </body>
