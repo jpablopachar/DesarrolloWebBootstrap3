0956666d439f4545ef468dc3a2b5eb15ec74fdff
Juan Pablo Pachar Viñan <jpablopachar1993@gmail.com>
2016-09-28 15:56:45 -0500

    Llamada del archivo styles.css en headCommon.php y se ingresa contenido general en el Css

--- inc/headCommon.php
+++ inc/headCommon.php
@@ -1,1 +1,3 @@
 <link rel="stylesheet" href="css/bootstrap.min.css">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+<link rel="stylesheet" href="css/styles.css">
