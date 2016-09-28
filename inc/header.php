8522c9570ad38acd5e2624c97dda9e121ddaf854
Juan Pablo Pachar Viñan <jpablopachar1993@gmail.com>
2016-09-28 16:33:18 -0500

    Se agrego estilos para la cabecera y barra de navegación

--- inc/header.php
+++ inc/header.php
@@ -1,22 +1,24 @@
-<div class="container">
-    <div class="row">
-        <div class="col-xs-3">
-            <h1 id="main-logo"><a href="index.php">WKSP <span>Workshops demo site</span></a></h1>
-        </div>
-        <div class="col-xs-9">
-            <ul id="main-menu" class="nav nav-pills">
-                <li class="active"><a href="index.php">INICIO</a></li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" >WORKSHOPS <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="detail.php">WORKSHOP A</a></li>
-                        <li><a href="detail.php">WORKSHOP B</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">INSTALACIONES</a></li>
-                <li><a href="#">FAQ</a></li>
-                <li><a href="#">CONTACTO</a></li>
-            </ul>
+<header id="main-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-3">
+                <h1 id="main-logo"><a href="index.php">WKSP <span>Workshops demo site</span></a></h1>
+            </div>
+            <div class="col-xs-9">
+                <ul id="main-menu" class="nav nav-pills">
+                    <li class="active"><a href="index.php">INICIO</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" >WORKSHOPS <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="detail.php">WORKSHOP A</a></li>
+                            <li><a href="detail.php">WORKSHOP B</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">INSTALACIONES</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">CONTACTO</a></li>
+                </ul>
+            </div>
         </div>
     </div>
-</div>
+</header>
