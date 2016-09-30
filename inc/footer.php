bcd922aa83aafd7604c2255dcb019ef9e1f1ce20
Juan Pablo Pachar Viñan <jpablopachar1993@gmail.com>
2016-09-30 14:39:06 -0500

    Damos estilos al footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,37 +1,39 @@
-<div class="container">
-    <div class="row">
-        <div class="col-xs-5">
-            <h3>PRÓXIMOS WORKSHOPS</h3>
-            <ul id="next-workshops">
-                <li><a href="detail.php">Título del primer workshop de pruebas</a></li>
-                <li><a href="detail.php">Título del segundo workshop de prueba</a></li>
-            </ul>
-        </div>
-        <div class="col-xs-6 col-xs-offset-1">
-            <h3>NO TE PIERDAS NUESTRAS NOVEDADES</h3>
-            <form action="" id="subscribe-form">
-                <div class="row">
-                    <div class="col-xs-8">
-                        <div class="form-group">
-                            <label for="email" class="sr-only">tu correo electrónico</label>
-                            <input type="text" id="email" class="form-control input-lg" name="email" placeholder="tu correo electrónico">
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-5">
+                <h3>PRÓXIMOS WORKSHOPS</h3>
+                <ul id="next-workshops">
+                    <li><a href="detail.php">Título del primer workshop de pruebas</a></li>
+                    <li><a href="detail.php">Título del segundo workshop de prueba</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-6 col-xs-offset-1">
+                <h3>NO TE PIERDAS NUESTRAS NOVEDADES</h3>
+                <form action="" id="subscribe-form">
+                    <div class="row">
+                        <div class="col-xs-8">
+                            <div class="form-group">
+                                <label for="email" class="sr-only">tu correo electrónico</label>
+                                <input type="text" id="email" class="form-control input-lg" name="email" placeholder="tu correo electrónico">
+                            </div>
+                        </div>
+                        <div class="col-xs-4">
+                            <button class="btn btn-custom btn-lg">SUSCRIBIRME</button>
                         </div>
                     </div>
-                    <div class="col-xs-4">
-                        <button class="btn btn-custom btn-lg">SUSCRIBIRME</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div class="clearflix"></div>
-        <div class="col-xs-5">
-            <h3>SOBRE LOS WORKSHOPS</h3>
-            <p>Estos workshops son el resultado de la suma de la ilusión y esfuerzos de <a href="#">Empresas</a>, <a href="#">Empresa 2</a> y <a href="#">Persona</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, tempora, aspernatur. Nam debitis placeat dignissimos repellendus, esse libero cum sapiente culpa eius, incidunt pariatur.</p>
-        </div>
-        <div class="col-xs-6 col-xs-offset-1">
-            <h3>TE GUSTA? CORRE LA VOZ</h3>
-            <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, nam, adipisci? Amet, deserunt voluptatibus. Quos ad, optio quod? Soluta, molestias.</P>
+                </form>
+            </div>
+            <div class="clearflix"></div>
+            <div class="col-xs-5">
+                <h3>SOBRE LOS WORKSHOPS</h3>
+                <p>Estos workshops son el resultado de la suma de la ilusión y esfuerzos de <a href="#">Empresas</a>, <a href="#">Empresa 2</a> y <a href="#">Persona</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, tempora, aspernatur. Nam debitis placeat dignissimos repellendus, esse libero cum sapiente culpa eius, incidunt pariatur.</p>
+            </div>
+            <div class="col-xs-6 col-xs-offset-1">
+                <h3>TE GUSTA? CORRE LA VOZ</h3>
+                <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, nam, adipisci? Amet, deserunt voluptatibus. Quos ad, optio quod? Soluta, molestias.</P>
+            </div>
         </div>
     </div>
-</div>
+</footer>
