9991a1a9b132077333d6a1a5bbcab3bceea17fb6
Juan Pablo Pachar Viñan <jpablopachar1993@gmail.com>
2016-10-03 18:13:22 -0500

    Se arregla el sistema de rejillas de bootstrap para contenido movil

--- index.php
+++ index.php
@@ -12,16 +12,16 @@
             <article id="w1" class="workshop workshop-left">
                 <div class="container">
                     <div class="row">
-                        <div class="col-xs-6 main-info">
+                        <div class="col-xs-12 col-sm-3 col-sm-push-9 col-md-4 col-md-push-8 col-lg-6 col-lg-push-6 date-info">
+                            <h3 class="highlighted"><strong>W21</strong> 13/14 de abril</h3>
+                            <h3 class="highlighted">20 plazas</h3>
+                            <h3 class="highlighted">$ 99</h3>
+                        </div>
+                        <div class="col-xs-12 col-sm-9 col-sm-pull-3 col-md-8 col-md-pull-4 col-lg-6 col-lg-pull-6 main-info">
                             <h2>Desarrolla front-end like a boss aprendiendo Bootstrap 3</h2>
                             <p>Disfruta de un fin de semana diferente aprendiendo con <a href="#">Juan Pablo Pachar</a> y <a href="#">Junior Vásquez</a>.</p>
                             <a href="detail.php" class="btn btn-custom">ME APUNTO!</a>
                             <a href="detail.php" class="more-info-link">quiero más información</a>
-                        </div>
-                        <div class="col-xs-4 col-xs-offset-2 date-info">
-                            <h3 class="highlighted"><strong>W21</strong> 13/14 de abril</h3>
-                            <h3 class="highlighted">20 plazas</h3>
-                            <h3 class="highlighted">$ 99</h3>
                         </div>
                     </div>
                 </div>
@@ -29,12 +29,12 @@
             <article id="w2" class="workshop workshop-right">
                 <div class="container">
                     <div class="row">
-                        <div class="col-xs-4 date-info">
+                        <div class="col-xs-12 col-sm-3 col-md-4 col-lg-6 date-info">
                             <h3 class="highlighted"><strong>W38</strong> 19/20 de abril</h3>
                             <h3 class="highlighted">15 plazas</h3>
                             <h3 class="highlighted">$ 120</h3>
                         </div>
-                        <div class="col-xs-6 col-xs-offset-2 main-info">
+                        <div class="col-xs-12 col-sm-9 col-md-8 col-lg-6 main-info">
                             <h2>Desarrolla apliaciones multiplataforma con Adobe AIR</h2>
                             <p>Sorpréndete de lo fácil que puede ser crear apps para iOS y Android con este workshop impartido por <a href="#">Juan Pablo Pachar</a></p>
                             <a href="detail.php" class="more-info-link">quiero más información</a>
